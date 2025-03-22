3f9acc2ab2fcd2ce9ad275380f2432ead1703d98
SamuseuDzmitry <hloop121@gmail.com>
2025-03-23 00:31:33 +0300

    add TimSort

---
+++ src/model/Barrel.java
@@ -0,0 +1,76 @@
+package model;
+
+public class Barrel implements Comparable<Barrel> {
+
+    private int volume;
+    private String storedMaterial;
+    private String material;
+
+    private Barrel(Builder builder) {
+        this.volume = builder.volume;
+        this.storedMaterial = builder.storedMaterial;
+        this.material = builder.material;
+    }
+
+    public Barrel() {
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public String getStoredMaterial() {
+        return storedMaterial;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public static Builder getBuilder() {
+        return new Barrel().new Builder();
+    }
+
+    @Override
+    public int compareTo(Barrel other) {
+        if (this.volume != other.volume)
+            return Integer.compare(this.volume, other.volume);
+        if (!this.storedMaterial.equalsIgnoreCase(other.storedMaterial))
+            return this.storedMaterial.compareTo(other.storedMaterial);
+        return this.material.compareTo(other.material);
+    }
+
+    @Override
+    public String toString() {
+        return "Barrel{" +
+                "volume=" + volume +
+                ", storedMaterial='" + storedMaterial + '\'' +
+                ", material='" + material + '\'' +
+                '}';
+    }
+
+    public class Builder {
+        private int volume;
+        private String storedMaterial;
+        private String material;
+
+        public Builder setVolume(int volume) {
+            this.volume = volume;
+            return this;
+        }
+
+        public Builder setStoredMaterial(String storedMaterial) {
+            this.storedMaterial = storedMaterial;
+            return this;
+        }
+
+        public Builder setMaterial(String material) {
+            this.material = material;
+            return this;
+        }
+
+        public Barrel build() {
+            return new Barrel(this);
+        }
+    }
+}
---
+++ src/util/TrialSort.java
@@ -0,0 +1,103 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrialSort {
+
+    private static int minRun(int listLenght)
+    {
+        int start = 0;
+        while (listLenght >= 64) {
+            start |= listLenght & 1;
+            listLenght >>= 1;
+        }
+        return listLenght + start;
+    }
+
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    private static void insertionSort(Object[] array, int left, int right) {
+        for (int i = left + 1; i <= right; i++) {
+            Object tempI = array[i];
+            int j = i - 1;
+            Comparable key = (Comparable) array[j];
+            while (j >= 0 && key.compareTo(tempI) > 0 && j >= left) {
+                array[j + 1] = array[j];
+                j--;
+                key = j >= 0 ? (Comparable) array[j] : null;
+            }
+            array[j + 1] = tempI;
+        }
+    }
+
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    private static void merge(Object[] array, int left, int mid, int right) {
+
+        int leftArrLen = mid - left + 1;
+        int rightArrLen = right - mid;
+        Object[] leftArr = new Object[leftArrLen];
+        Object[] rightArr = new Object[rightArrLen];
+
+        System.arraycopy(array, left, leftArr, 0, leftArrLen);
+        for (int x = 0; x < rightArrLen; x++) {
+            rightArr[x] = array[mid + 1 + x];
+        }
+
+        int i = 0;
+        int j = 0;
+        int k = left;
+
+        while (i < leftArrLen && j < rightArrLen) {
+            Comparable tmp = (Comparable) leftArr[i];
+            if (tmp.compareTo(rightArr[j]) <= 0) {
+                array[k] = leftArr[i];
+                i++;
+            } else {
+                array[k] = rightArr[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < leftArrLen) {
+            array[k] = leftArr[i];
+            k++;
+            i++;
+        }
+
+        while (j < rightArrLen) {
+            array[k] = rightArr[j];
+            k++;
+            j++;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T extends Comparable<T>> List<T> sort(List<T> list) {
+
+        Object[] array = list.toArray();
+
+
+        int length = list.size();
+        int run = minRun(length);
+
+        for (int i = 0; i < length; i += run) {
+
+            insertionSort(array, i, Math.min((i + 31), (length - 1)));
+        }
+
+        for (int size = run; size < length; size = 2 * size) {
+            for (int left = 0; left < length; left += 2 * size) {
+                int mid = left + size - 1;
+                int right = Math.min((left + 2 * size - 1), (length - 1));
+                merge(array, left, mid, right);
+            }
+        }
+
+        List<T> sortArray = new ArrayList<>();
+        for (Object e : array) {
+            sortArray.add((T) e);
+        }
+        return sortArray;
+    }
+}
